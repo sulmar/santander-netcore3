a67ec8c79e1df0e85820e522e9edfa0b66e7003f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-03-22 15:19:56 +0100

    Utworzenie własnych konfiguracji

--- Shopper.WebApi/Program.cs
+++ Shopper.WebApi/Program.cs
@@ -16,11 +16,29 @@ namespace Shopper.WebApi
             CreateHostBuilder(args).Build().Run();
         }
 
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        });
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    string environmentName = hostingContext.HostingEnvironment.EnvironmentName;
+
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    config.AddXmlFile("appsettings.xml", optional: true);
+                    config.AddJsonFile($"appsettings.{environmentName}.json", optional: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+
+
     }
 }
