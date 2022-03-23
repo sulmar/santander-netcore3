a01b78a33cb957e7fbcc9237279b02575a2b868a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-03-23 15:14:41 +0100

    gRPC

---
+++ Shopper.DeliveryConsoleClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using Bogus;
+using Grpc.Net.Client;
+using Shopper.DeliveryService;
+using System;
+using System.Threading.Tasks;
+
+namespace Shopper.DeliveryConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            const string url = "https://localhost:5001";
+
+            var channel = GrpcChannel.ForAddress(url);
+
+            Shopper.DeliveryService.ShippingService.ShippingServiceClient client = new DeliveryService.ShippingService.ShippingServiceClient(channel);
+
+            var faker = new Faker<ConfirmDeliveryRequest>()
+                .RuleFor(p => p.OrderId, f => f.IndexFaker)
+                .RuleFor(p => p.Sign, f => f.Lorem.Word());
+
+            var requests = faker.GenerateForever();
+
+            foreach (var request in requests)
+            {
+                Console.Write($"Send {request.OrderId} {request.Sign} Status: " );
+
+                var response = await client.ConfirmDeliveryAsync(request);
+
+                Console.WriteLine(response.IsConfirmed);
+
+                await Task.Delay(TimeSpan.FromSeconds(0.1));
+
+                
+            }
+
+            
+        }
+    }
+}
---
+++ Shopper.DeliveryService/Services/ShippingService.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopper.DeliveryService.Services
+{
+    public class ShippingService : Shopper.DeliveryService.ShippingService.ShippingServiceBase
+    {
+        private readonly ILogger<ShippingService> logger;
+
+        public ShippingService(ILogger<ShippingService> logger)
+        {
+            this.logger = logger;
+        }
+
+        public override Task<ConfirmDeliveryReply> ConfirmDelivery(ConfirmDeliveryRequest request, ServerCallContext context)
+        {
+            logger.LogInformation("Confirmed {0} {1}", request.OrderId, request.Sign);
+
+            ConfirmDeliveryReply reply = new ConfirmDeliveryReply { IsConfirmed = true };
+
+            return Task.FromResult(reply);
+            
+        }
+    }
+}
